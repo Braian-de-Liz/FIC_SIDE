63a2c7f4b2da6d8f842d529141a8aaabdd131a2a
Braian-de-Liz <delizbraian@gmail.com>
2026-03-07 17:44:47 -0300

    rota de health

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger"
 
 import { dbModel } from "./lib/drizzle_plugin";
+import { health_route } from "./routes/health/health"
 import { user_plugin } from "./routes/user/user_plugins";
 
 const Server = new Elysia()
     .use(dbModel)
     .use(user_plugin)
+    .use(health_route)
     .use(swagger())
     .listen(3334);
 
